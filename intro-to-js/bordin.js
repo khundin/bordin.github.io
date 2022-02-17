e2acd1dc7cf90ce57d93691fc9ed81d5568d701d
bordin tharwee in <coondin@hotmail.com>
2022-02-17 14:24:37 +0700

    commit 1111111

--- intro-to-js/bordin.js
+++ intro-to-js/bordin.js
@@ -2,11 +2,11 @@ function askMyName() {
     const name = prompt("Please enter your name");
  
     if (name != null) {
-       document.getElementById("textname").innerHTML = "My name is " + name;
+       document.getElementById("text-name").innerHTML = "My name is " + name;
     }
  }
  
  function helloWorld() {
- const h1message = document.getElementById("textname").innerHTML
+ const h1message = document.getElementById("text-name").innerHTML
     alert(h1message + '  ไม่มีหรอกครับจ่า ผมโยนเข้าป่าหมดแล้ว');
  }
