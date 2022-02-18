498366c02be442ac3d6bcde1eb32565b7463c70e
bordin tharwee in <coondin@hotmail.com>
2022-02-18 13:53:08 +0700

    add exercise 18-02-2022

--- intro-to-js/bordin.js
+++ intro-to-js/bordin.js
@@ -10,3 +10,15 @@ function askMyName() {
  const h1message = document.getElementById("text-name").innerHTML
     alert(h1message + '  ไม่มีหรอกครับจ่า ผมโยนเข้าป่าหมดแล้ว');
  }
+
+function calAge() {
+   const birthyear = prompt("Please enter your Birth year");
+   alert ("ยินดีด้วย นายอยู่ประเทศนี้มา " + (new Date().getFullYear()-birthyear) + " ปีแล้ว");
+}
+
+function loadProfile() {
+   const name = "Bordin Tharwee In"
+   const province = "Surin, Thailand"
+   document.getElementById("my-name").innerHTML = name;
+   document.getElementById("my-province").innerHTML = province;
+}
